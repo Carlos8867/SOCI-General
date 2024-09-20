959c2e5e0958c417864da1788330ba178f48ea6e
CARLOS PANTOJA <cepabco080867@gmailcom>
2024-09-20 13:16:00 -0500

    first commit

---
+++ vistas/home.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<div class="container is-fluid">
+<meta charset="utf-8">
+	<h1 class="title">Home</h1>
+	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+	<meta charset="utf-8">
+
+</div>
